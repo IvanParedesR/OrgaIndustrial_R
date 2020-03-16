91569ad922a451e1f0c4108fa0b7dd2c0600b2d2
Iván Paredes Reséndiz <ivanresendiz25@gmail.com>
2020-03-16 08:40:57 -0600

    Update Game Theory.R

--- Game Theory.R
+++ Game Theory.R
@@ -2,3 +2,17 @@ install.packages("GameTheory")
 library(GameTheory)
 https://cran.r-project.org/web/packages/GameTheory/vignettes/GameTheory.pdf
 https://readthedocs.org/projects/nashpy/downloads/pdf/stable/
+
+
+Nashpy Documentation, Release 0.0.18To summarise:•  
+If you want to do sophisticated efficient game theoretic computations, use Gambit.
+If you are happy to use a GUI use Game Theory Explorer.
+If you would like an easy to install Python library for two player games you can useNashpy.
+
+4.3  Other Python Game theory libraries
+Axelrod: a research library aimed at the study of the Iterated Prisoners dilemma [Knight2016].
+Gambit:  a C library with a Python interface for the computation of equilibria [McKelvey2016].How doesNashpy relate to Gambit.
+Game theory explorer a web interface to gambit useful for teaching. [Savani2015] 
+PyNFG: PyNFG is a Python package for modeling and solving Network Form Games.
+lrslib: A C implementation of a reverse search algorithm with modules for Nash equilibria computation.  
+sagemath: The mathematical software package Sage has various algorithms for the computation of Nash equi-libr
