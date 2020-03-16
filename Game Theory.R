c15f5c15757fd37515732a49a4ad19557b440c28
Iván Paredes Reséndiz <ivanresendiz25@gmail.com>
2020-03-15 19:27:12 -0600

    Update Game Theory.R

--- Game Theory.R
+++ Game Theory.R
@@ -1,2 +1,4 @@
 install.packages("GameTheory")
 library(GameTheory)
+https://cran.r-project.org/web/packages/GameTheory/vignettes/GameTheory.pdf
+https://readthedocs.org/projects/nashpy/downloads/pdf/stable/
